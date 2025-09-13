9ea9c2d494d3e27bb705624b0ad7734699e8bc04
Ketan Gathibandhe <ketangathibandhe04@gmail.com>
2025-09-13 15:52:43 +0530

    /

--- src/components/SkillGapForm.jsx
+++ src/components/SkillGapForm.jsx
@@ -71,7 +71,7 @@ const SkillGapForm = () => {
             w-full max-w-[95%] sm:max-w-[520px] md:max-w-[650px] lg:max-w-[800px] xl:max-w-[900px]
             mx-auto
             rounded-xl
-            sm:min-h-[550px] md:min-h-[550px] lg:min-h-[500px]
+            sm:min-h-[500px] md:min-h-[500px] lg:min-h-[480px]
           "
         >
           <div className="card-body">
