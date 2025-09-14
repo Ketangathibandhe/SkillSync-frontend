dd695840970df434fd88082a411c0539023e13cb
Ketan Gathibandhe <ketangathibandhe04@gmail.com>
2025-09-14 13:30:50 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,151 +1,3 @@
-// import "./App.css";
-// import {
-//   BrowserRouter,
-//   Route,
-//   Routes,
-//   Outlet,
-//   Navigate,
-// } from "react-router-dom";
-// import { Provider, useDispatch, useSelector } from "react-redux";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// import appStore from "./utils/appStore";
-// import { addUser } from "./utils/userSlice";
-// import { BASE_URL } from "./utils/constants";
-
-// // Pages
-// import Login from "./components/Login";
-// import EditProfile from "./components/EditProfile";
-// import ForgotPasswordForm from "./components/ForgotPasswordForm";
-// import RoadmapPage from "./components/RoadmapPage";
-// import SkillGapForm from "./components/SkillGapForm";
-// import ProtectedRoute from "./components/ProtectedRoute";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-
-// // Layout with Navbar + Footer
-// const Layout = () => (
-//   <>
-//     <Navbar />
-//     <Outlet />
-//     <Footer />
-//   </>
-// );
-
-// // Redirect away from public pages if already logged in
-// const PublicPage = ({ children }) => {
-//   const user = useSelector((state) => state.user);
-//   if (user && user.emailId) return <Navigate to="/skillGapForm" replace />;
-//   return children;
-// };
-
-// // Load auth state once for the whole app (refresh-safe)
-// const AuthLoader = ({ children }) => {
-//   const dispatch = useDispatch();
-//   const user = useSelector((state) => state.user);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     let mounted = true;
-//     const fetchUser = async () => {
-//       try {
-//         const res = await axios.get(`${BASE_URL}/api/profile/profile/view`, {
-//           withCredentials: true,
-//         });
-//         if (mounted) dispatch(addUser(res.data));
-//       } catch {
-//         // not logged in is fine
-//       } finally {
-//         if (mounted) setLoading(false);
-//       }
-//     };
-
-//     // If user already set (e.g., just signed up or logged in), skip fetch to avoid race
-//     if (!user || !user.emailId) {
-//       fetchUser();
-//     } else {
-//       setLoading(false);
-//     }
-
-//     return () => {
-//       mounted = false;
-//     };
-//   }, [dispatch, user]);
-
-//   if (loading) {
-//     return (
-//       <div className="w-full h-screen flex justify-center items-center text-lg">
-//         Loading...
-//       </div>
-//     );
-//   }
-
-//   return children;
-// };
-
-// function App() {
-//   return (
-//     <Provider store={appStore}>
-//       <BrowserRouter basename="/">
-//         <AuthLoader>
-//           <Routes>
-//             <Route path="/" element={<Layout />}>
-//               {/* Public routes */}
-//               <Route
-//                 path="login"
-//                 element={
-//                   <PublicPage>
-//                     <Login />
-//                   </PublicPage>
-//                 }
-//               />
-//               {/* Forgot password public for both logged-in and logged-out */}
-//               <Route path="forgotpassword" element={<ForgotPasswordForm />} />
-
-//               {/* Protected routes */}
-//               <Route
-//                 index
-//                 element={
-//                   <ProtectedRoute>
-//                     <SkillGapForm />
-//                   </ProtectedRoute>
-//                 }
-//               />
-//               <Route
-//                 path="skillGapForm"
-//                 element={
-//                   <ProtectedRoute>
-//                     <SkillGapForm />
-//                   </ProtectedRoute>
-//                 }
-//               />
-//               <Route
-//                 path="profile"
-//                 element={
-//                   <ProtectedRoute>
-//                     <EditProfile />
-//                   </ProtectedRoute>
-//                 }
-//               />
-//               <Route
-//                 path="roadmap"
-//                 element={
-//                   <ProtectedRoute>
-//                     <RoadmapPage />
-//                   </ProtectedRoute>
-//                 }
-//               />
-//             </Route>
-//           </Routes>
-//         </AuthLoader>
-//       </BrowserRouter>
-//     </Provider>
-//   );
-// }
-
-// export default App;
-
-
 import "./App.css";
 import {
   BrowserRouter,
@@ -158,7 +10,7 @@ import { Provider, useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import appStore from "./utils/appStore";
-import { addUser, removeUser } from "./utils/userSlice";
+import { addUser } from "./utils/userSlice";
 import { BASE_URL } from "./utils/constants";
 
 // Pages
@@ -197,17 +49,18 @@ const AuthLoader = ({ children }) => {
     let mounted = true;
     const fetchUser = async () => {
       try {
-        const res = await axios.get(`${BASE_URL}/api/auth/check-auth`, {
+        const res = await axios.get(`${BASE_URL}/api/profile/profile/view`, {
           withCredentials: true,
         });
         if (mounted) dispatch(addUser(res.data));
       } catch {
-        if (mounted) dispatch(removeUser());
+        // not logged in is fine
       } finally {
         if (mounted) setLoading(false);
       }
     };
 
+    // If user already set (e.g., just signed up or logged in), skip fetch to avoid race
     if (!user || !user.emailId) {
       fetchUser();
     } else {
@@ -246,6 +99,7 @@ function App() {
                   </PublicPage>
                 }
               />
+              {/* Forgot password public for both logged-in and logged-out */}
               <Route path="forgotpassword" element={<ForgotPasswordForm />} />
 
               {/* Protected routes */}
@@ -290,3 +144,5 @@ function App() {
 }
 
 export default App;
+
+
